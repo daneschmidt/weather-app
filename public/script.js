abc431783ad98f9b2eefcf4678f4c1b73ade3eb7
unknown <daneschmidt@gmail.com>
2020-03-03 21:48:51 -0600

    data is hitting the page

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement)
+
 searchBox.addListener('places_changed', () => {
     const place = searchBox.getPlaces()[0]
     if (place == null) return
@@ -17,6 +18,20 @@ searchBox.addListener('places_changed', () => {
         })
     }).then(res => res.json()).then(data => {
         console.log(data)
-        // setWeatherData(data, place.formatted_address)
+        setWeatherData(data, place.formatted_address)
     })
 })
+
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const windElement = document.querySelector('[data-wind]')
+const tempElement = document.querySelector('[data-temprature]')
+const precipElement = document.querySelector('[data-precipitation]')
+
+function setWeatherData(data, place) {
+    locationElement.textContent = place
+    statusElement.textContent = data.summary
+    tempElement.textContent = data.temperature
+    precipElement.textContent = `${data.precipProbability * 100}%`
+    windElement.textContent = data.windSpeed
+}
